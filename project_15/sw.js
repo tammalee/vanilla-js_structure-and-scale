123beb0d94026c55eb349c0badc715de22d86c6e
Tammy Valgardson <tammalee@gmail.com>
2022-05-07 14:16:35 -0600

    Update offline message. Add fetch eventListener to service worker

--- project_15/sw.js
+++ project_15/sw.js
@@ -7,3 +7,21 @@ self.addEventListener('install', function (event) {
         return cache;
     }));
 });
+
+self.addEventListener('fetch', function (event) {
+    let request = event.request;
+
+    // Bug fix
+	// https://stackoverflow.com/a/49719964
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+    
+    if (request.headers.get('Accept').includes('text/html')) {
+        event.respondWith(
+            fetch(request).then(function (response) {
+                return response;
+            }).catch(function (error) {
+                return caches.match('offline.html');
+            })
+        )
+    }
+});
